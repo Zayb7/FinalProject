7f3cab7a49b17da0df8fd72d10d1105031a513ba
Hunter Lang <hlang@mymail.mines.edu>
2013-04-28 20:53:45 -0600

    interesting gui

--- FinalProject/src/game/Game.java
+++ FinalProject/src/game/Game.java
@@ -1,9 +1,15 @@
 package game;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 @SuppressWarnings("serial")
 public class Game extends JFrame {
@@ -20,7 +26,7 @@ public class Game extends JFrame {
 		
 		setTitle("Mustard Bubbles");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(800, 768);
+		setSize(580, 750);
 		setVisible(true);
 		
 	}
@@ -36,6 +42,27 @@ public class Game extends JFrame {
 	
 	private JPanel controlPanel() {
 		JPanel controlPanel = new JPanel();
+		controlPanel.setLayout(new GridLayout(0, 1));
+		JButton start = new JButton("Start Game");
+		JButton stop = new JButton("STAHP");
+		JButton reset = new JButton("Reset dat bitch");
+		
+		JPanel scorePanel = new JPanel();
+		JTextField score = new JTextField("Score this madafaka");
+		JTextField timer = new JTextField("0");
+		timer.setEditable(false);
+		score.setEditable(false);
+		scorePanel.setLayout(new GridLayout(0, 1));
+		scorePanel.add(score);
+		scorePanel.add(timer, BorderLayout.CENTER);
+		scorePanel.setBorder(new TitledBorder(new EtchedBorder(), "Score and Time"));
+		
+		
+		controlPanel.add(start);
+		controlPanel.add(stop);
+		controlPanel.add(reset);
+		controlPanel.add(scorePanel);
+		controlPanel.setBorder(new TitledBorder(new EtchedBorder(), "Controls"));
 		
 		
 		return controlPanel;
